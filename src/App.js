fe9ddd499c1394cf014e0f6a55548a37065b2281
Mos3aB696 <mos3ab696@gmail.com>
2025-01-30 19:58:15 +0200

    Add the unique key to BillSplit Component to reset values inside inputs if change to another friend

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ function App() {
         <BillSplit
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
